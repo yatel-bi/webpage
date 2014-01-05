e999876da0b54cd06bb748c699e969f88cbd00bd
Juan BC <jbc.develop@gmail.com>
2014-01-05 19:47:59 -0300

    fixed logo

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Yatel - Network BI'
+project = u'Yatel BI'
 copyright = u'2010, 2011, 2012, 2013, 2014, Yatel Team'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -131,7 +131,7 @@ html_title = "Yatel BI"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/logo_24x24.png"
+html_logo = "_static/logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
