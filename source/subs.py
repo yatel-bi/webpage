d5808710e555b4b28d9f221cc887467d3f15e0c3
Juan BC <jbc.develop@gmail.com>
2014-01-07 00:12:05 -0300

    end install docs

---
+++ source/subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+SUBSTITUTIONS = {
+    "YATEL_DOC": 'http://yatel.readthedocs.org/',
+    "YATEL_FORUM": 'https://groups.google.com/forum/#!forum/utn_kdd',
+
+    "YATEL_OLAP_REPO": 'https://bitbucket.org/yatel/yatel',
+    "YATEL_OLAP_ISSUES": 'https://bitbucket.org/yatel/yatel/issues',
+    "YATEL_OLAP_DOWNLOADS": 'https://bitbucket.org/yatel/yatel/downloads',
+    "YATEL_WEBPAGE_REPO": 'https://bitbucket.org/yatel/webpage',
+    "YATEL_WEBPAGE_ISSUES": 'https://bitbucket.org/yatel/webpage/issues',
+
+
+}
